f7c012580811092db14c661fc03e56d847293b91
Duarte Cruz <102768606+DuarteCruz6@users.noreply.github.com>
2023-10-22 17:57:46 +0100

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -1,16 +1,24 @@
 global stringletras
-stringletras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+stringletras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  #Esta string irá ser utilizada para ver qual a coluna da interseção
 
 
-def eh_territorio(x):
-    if type(x) != tuple:
+def eh_territorio(t):       
+    """
+    Verifica se o território é válido.
+    Para o território ser válido tem de ser um tuplo de tuplos, com tamanho positivo inferior a 27 e cujos valores ou é 0 ou 1.
+
+    :param t: tuplo
+    :return: boolean
+    """
+
+    if type(t) != tuple:    
         return False
-    elif len(x)>26 or len(x)<1:
+    elif len(t)>26 or len(t)<1:
         return False
-    for i in x:
+    for i in t:
         if type(i)!= tuple:
             return False
-        elif len(x[len(x)-1]) != len(i):
+        elif len(t[len(t)-1]) != len(i):
             return False
         elif len(i)>99 or len(i)<1:
             return False 
@@ -19,12 +27,27 @@ def eh_territorio(x):
                 return False
     return True
 
-def obtem_ultima_intersecao(x):
-    letra = stringletras[len(x)-1]
-    num = len(x[0])
+def obtem_ultima_intersecao(t):      
+    """
+    Obtem a última interseção do território
+
+    :param t: tuplo
+    :return: tuplo
+    """
+
+    letra = stringletras[len(t)-1]
+    num = len(t[0])
     return (letra,num)
 
-def eh_intersecao(x):
+def eh_intersecao(x):       
+    """
+    Verifica se a interseção é válida.
+    Para ser válida, tem de corresponder a um tuplo com uma letra de A a Z e um número de 1 a 99.
+
+    :param x: tuplo
+    :return: boolean
+    """
+
     if type(x)!= tuple:
         return False
     if len(x)!=2:
@@ -40,20 +63,39 @@ def eh_intersecao(x):
                     return True
     return False
 
-def eh_intersecao_valida(t,i):
-    if eh_intersecao(i)== False:
+def eh_intersecao_valida(t,x):      
+    """
+    Verifica se a interseção é válida e se pertence ao território introduzido.
+    Para ser válida, tem de corresponder a um tuplo com uma letra de A a Z e um número de 1 a 99.
+
+    :param t: tuplo
+    :param x: tuplo
+    :return: boolean
+    """
+
+    if eh_intersecao(x)== False:
         return False
-    letra = i[0]
-    num = i[1]
+    letra = x[0]
+    num = x[1]
     if stringletras.index(letra) < len(t):
         if num<= len(t[0]):
             return True
     return False
 
-def eh_intersecao_livre(t,i):
-    letra = i[0]
+def eh_intersecao_livre(t,x):       
+    """
+    Verifica se a interseção corresponde a uma montanha ou não.
+    Para corresponder a uma montanha, o seu valor tem de ser 1.
+    Para ser uma interseção livre, o seu valor tem de ser 0.
+
+    :param t: tuplo
+    :param x: tuplo
+    :return: boolean
+    """
+
+    letra = x[0]
     coluna = stringletras.index(letra)
-    num = i[1]-1
+    num = x[1]-1
     sitio = t[coluna]
     montanha = sitio[num]
     if montanha == 1:
@@ -61,10 +103,19 @@ def eh_intersecao_livre(t,i):
     else:
         return True
 
-def obtem_intersecoes_adjacentes(t,i):
+def obtem_intersecoes_adjacentes(t,x):      
+    """
+    Obtem as interseções adjacentes por ordem de leitura do território.
+    A ordem de leitura do território é: Baixo, Esquerda, Direita, Cima. 
+
+    :param t: tuplo
+    :param x: tuplo
+    :return: tuplo
+    """
+
     res = ()
-    letra = i[0]
-    num = i[1]
+    letra = x[0]
+    num = x[1]
     coluna = stringletras.index(letra)
     num_max = len(t[0])
     coluna_max = len(t)-1
@@ -78,26 +129,47 @@ def obtem_intersecoes_adjacentes(t,i):
         res += ((letra,num+1),)
     return res
 
-def ordenacao(i):
-    return i[1], i[0]
+def ordenacao(x):       
+    """
+    Função funcional para a chave da função sorted (que se encontra em ordena_intersecoes()).
 
-def ordena_intersecoes(t):
-    return tuple(sorted(t, key = ordenacao))
+    :param i: tuplo
+    :return: tuplo
+    """
 
-def territorio_para_str(t):
-    if type(t)!= tuple:
-        raise ValueError ("territorio_para_str: argumento invalido")
+    return x[1], x[0]
+
+def ordena_intersecoes(x):     
+    """
+    Ordena as interseções por ordem de leitura do território.
+    A ordem de leitura do território é: Baixo, Esquerda, Direita, Cima. 
+
+    :param x: tuplo
+    :return: tuplo
+    """
+
+    return tuple(sorted(x, key = ordenacao))
+
+def territorio_para_str(t):      
+    """
+    Altera o território para uma string verificando se é um território válido.
+    Para o território ser válido tem de ser um tuplo de tuplos, com tamanho positivo inferior a 27 e cujos valores ou é 0 ou 1.
+
+    :param t: tuplo
+    :return: string
+    """
+
     if eh_territorio(t) == False:
         raise ValueError ("territorio_para_str: argumento invalido")
     num_letras = len(t)
     str = "   "
-    for i in range(num_letras-1):
+    for i in range(num_letras-1):           #Coloca as letras que aparecem na parte de cima por ordem
         str+= f"{stringletras[i]} "
     str += f"{stringletras[num_letras-1]}"
     fim = str
     str += "\n"
     num_colunas = len(t[0])
-    while num_colunas >= 10:                #alinhar o algarismo das unidades dos números com algarismos com os números de apenas um algarismo
+    while num_colunas >= 10:                #Alinha o algarismo das unidades dos números com algarismos com os números de apenas um algarismo
         str += f"{num_colunas} "
         for i in range(num_letras):
             if t[i][num_colunas-1]== 0:
@@ -108,7 +180,7 @@ def territorio_para_str(t):
         str += "\n"
         num_colunas -=1
 
-    while num_colunas > 0:
+    while num_colunas > 0:                  #Alinha os números de apenas um algarismo
         str += f" {num_colunas} "
         for i in range(num_letras):
             if t[i][num_colunas-1]== 0:
@@ -121,7 +193,18 @@ def territorio_para_str(t):
     str += fim
     return str
 
-def obtem_cadeia(t,x):
+def obtem_cadeia(t,x):          
+    """
+    Obtem a cadeia correspondente à respetiva interseção no respetivo território, verificando os seus argumentos.
+    Os argumentos são: t- território; x- interseção.
+    Para o território ser válido tem de ser um tuplo de tuplos, com tamanho positivo inferior a 27 e cujos valores ou é 0 ou 1.
+    Para ser válida, tem de corresponder a um tuplo com uma letra de A a Z e um número de 1 a 99. Além disso, tem de pertencer ao território.
+
+    :param t: tuplo
+    :param x: tuplo
+    :return: tuplo
+    """
+
     if eh_territorio(t) == False or eh_intersecao_valida(t,x) == False or eh_intersecao(x) == False:
         raise ValueError ("obtem_cadeia: argumentos invalidos")  
     cadeia = eh_intersecao_livre(t,x)
@@ -142,7 +225,18 @@ def obtem_cadeia(t,x):
                     
     return ordena_intersecoes(res)
 
-def obtem_vale(t,x):
+def obtem_vale(t,x):        
+    """
+    Obtem o vale da cadeia de montanhas correspondente no respetivo território, verificando os argumentos.
+    Os argumentos são: t- território; x- interseção.
+    Para o território ser válido tem de ser um tuplo de tuplos, com tamanho positivo inferior a 27 e cujos valores ou é 0 ou 1.
+    Para ser válida, tem de corresponder a um tuplo com uma letra de A a Z e um número de 1 a 99. Além disso, tem de pertencer ao território.
+
+    :param t: tuplo
+    :param x: tuplo
+    :return: tuplo
+    """
+
     if eh_territorio(t) == False or eh_intersecao_valida(t,x) == False or eh_intersecao(x) == False or eh_intersecao_livre(t,x) == True:
         raise ValueError ('obtem_vale: argumentos invalidos')
     montanha = obtem_cadeia(t,x)
@@ -154,7 +248,19 @@ def obtem_vale(t,x):
                 res += (a,)
     return ordena_intersecoes(res)
 
-def verifica_conexao(t,x1,x2):
+def verifica_conexao(t,x1,x2):      
+    """
+    Verifica se as interseções estão conectadas, ou seja, se estão na mesma cadeia no respetivo território, verificando os seus argumentos.
+    Os argumentos são: t- território; x1- interseção; x2- interseção.
+    Para o território ser válido tem de ser um tuplo de tuplos, com tamanho positivo inferior a 27 e cujos valores ou é 0 ou 1.
+    Para ser válida, tem de corresponder a um tuplo com uma letra de A a Z e um número de 1 a 99. Além disso, tem de pertencer ao território.
+
+    :param t: tuplo
+    :param x1: tuplo
+    :param x2: tuplo
+    :return: boolean
+    """
+
     if eh_territorio(t) == False or eh_intersecao_valida(t,x1) == False or eh_intersecao(x1) == False or eh_intersecao_valida(t,x2) == False or eh_intersecao(x2) == False:
         raise ValueError ('verifica_conexao: argumentos invalidos')
     cadeiax1 = eh_intersecao_livre(t,x1)
@@ -186,7 +292,16 @@ def verifica_conexao(t,x1,x2):
                 return False
     return False
 
-def calcula_numero_montanhas(t):
+def calcula_numero_montanhas(t):        
+    """
+    Calcula o número de montanhas de um território, verificando o seu argumento.
+    O argumento é: t- território.
+    Para o território ser válido tem de ser um tuplo de tuplos, com tamanho positivo inferior a 27 e cujos valores ou é 0 ou 1.
+    
+    :param t: tuplo
+    :return: integer
+    """
+
     if eh_territorio(t) == False:
         raise ValueError ("calcula_numero_montanhas: argumento invalido")
     num = 0
@@ -196,17 +311,26 @@ def calcula_numero_montanhas(t):
                 num +=1
     return num
 
-def calcula_numero_cadeias_montanhas(t):
+def calcula_numero_cadeias_montanhas(t):        
+    """
+    Calcula o número de cadeias de montanhas do respetivo território, verificando o seu argumento.
+    O argumento é: t- território.
+    Para o território ser válido tem de ser um tuplo de tuplos, com tamanho positivo inferior a 27 e cujos valores ou é 0 ou 1.
+
+    :param t: tuplo
+    :return: integer
+    """
+
     if eh_territorio(t) == False:
         raise ValueError ("calcula_numero_cadeias_montanhas: argumento invalido")
     porvisitar = []
     visitadas = []
     num = 0
-    for i in t:
-        letra = stringletras[t.index(i)]
+    for i in range(len(t)):
+        letra = stringletras[i]
         a = 0
-        while a < len(i):
-            if i[a] == 1:
+        while a < len(t[0]):
+            if t[i][a] == 1:
                 posicao = (letra,a+1)
                 porvisitar += [posicao,]
             a +=1
@@ -218,7 +342,16 @@ def calcula_numero_cadeias_montanhas(t):
             num += 1
     return num
 
-def calcula_tamanho_vales(t):
+def calcula_tamanho_vales(t):       
+    """
+    Calcula o tamanho dos vales do respetivo território, verificando o seu argumento
+    O argumento é: t- território.
+    Para o território ser válido tem de ser um tuplo de tuplos, com tamanho positivo inferior a 27 e cujos valores ou é 0 ou 1.
+
+    :param t: tuplo
+    :return: integer
+    """
+
     if eh_territorio(t) == False:
         raise ValueError ('calcula_tamanho_vales: argumento invalido')
     porvisitar = []
@@ -248,6 +381,3 @@ def calcula_tamanho_vales(t):
                 tamanho += 1
                 valevisto +=[a,]          
     return tamanho
-
-    
-
